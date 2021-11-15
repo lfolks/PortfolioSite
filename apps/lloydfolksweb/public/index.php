2b37b470cc1907b9043b2bd9288c033da04730da
Lloyd D. Folks Jr <lfolks@legacylearningsolutions.net>
2021-11-15 05:54:14 -0500

    First Page
    Adding a placeholder page for the start of lloydfolks.com

---
+++ apps/lloydfolksweb/public/index.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>Coming Soon!</title>
+    <meta charset="utf-8"/>
+    <style>
+        body {
+            margin: 0;
+            padding: 0;
+        }
+        #header {
+            background-color: #f1f1f1;
+            border-bottom: 1px solid #ddd;
+            padding: 30px;
+            text-align: center;
+            color: #444;
+            font-size: 22px;
+        }
+        #content {
+            padding: 30px;
+            text-align: center;
+            font-size: 18px;
+        }
+    </style>
+</head>
+<body>
+
+<div id="header">
+    lloydfolks.com is coming soon!
+</div>
+
+<div id="content">
+    Check back later to see what will be here!
+</div>
+
+</body>
+</html>
